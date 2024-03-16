71c63bf4c10bae96229a78243c19f79545f34118
RenMi1 <lorenmay.florentino@hcdc.edu.ph>
2024-03-17 00:37:13 +0800

    Add TypeScript

---
+++ Todolist/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { TodoProvider } from './TodoContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoProvider>
+      <App />
+    </TodoProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
